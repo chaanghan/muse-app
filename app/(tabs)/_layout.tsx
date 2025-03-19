57db31179c251118a95dcef36d5bbb991078df22
changhan <momo017097@gmail.com>
2025-03-19 20:25:01 +0900

    design: tab bar icon 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,29 +1,43 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+import { Tabs } from 'expo-router';
+import { colors } from '@/constants/colors';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Octicons from '@expo/vector-icons/Octicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: colors.BLACK,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: '홈',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home-sharp" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
           title: '검색',
+          tabBarIcon: ({ color }) => (
+            <Octicons name="search" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="library"
         options={{
           title: '내 라이브러리',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="library-music" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
