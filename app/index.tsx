4f79e403db0967b8d58591399ebf6f50b75e37cb
changhan <momo017097@gmail.com>
2025-05-06 22:57:26 +0900

    fix: 초기 네비게이션 렌더링 수정

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { useRootNavigationState, useRouter } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { useEffect } from 'react';
 
 export default function Root() {
   const router = useRouter();
-  const rootNavigation = useRootNavigationState();
 
   useEffect(() => {
-    if (!rootNavigation?.key) return; // 네비게이션 준비 전이면 리턴
-
-    router.replace('/(tabs)/home'); // 네비게이션이 준비된 후에 이동
-  }, [rootNavigation?.key]);
+    router.replace('/(tabs)/home');
+  }, []);
 
   return null;
 }
